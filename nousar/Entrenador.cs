a3ea71b6ca15a5e01f82c91f8045f44b34b09f87
Samuel-Fernandez-140923 <samueldukipkoko@gmail.com>
2024-10-17 08:57:27 -0500

    Cuarta salvcada

---
+++ nousar/Entrenador.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Personas
+{
+    internal class Entrenador
+    { 
+
+
+        
+            public string Nombre { get; set; }
+            public string Deporte { get; set; }
+
+            public void HaceEjercicio()
+            {
+                Console.WriteLine($"{Nombre} está haciendo ejercicio.");
+            }
+
+            public void EntrenaEstudiantes()
+            {
+                Console.WriteLine($"{Nombre} está entrenando a los estudiantes.");
+            }
+
+            public override string ToString()
+            {
+                return $"Entrenador: {Nombre}, Deporte: {Deporte}";
+            }
+        }
+    }
+
+
---
+++ nousar/alumno.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Personas
+{
+    internal class alumno
+    {
+        public class Estudiante
+        {
+            public string Nombre { get; set; }
+            public int Edad { get; set; }
+
+            public void Corre()
+            {
+                Console.WriteLine($"{Nombre} esta corriendo loco.");
+            }
+
+            public void Estudia()
+            {
+                Console.WriteLine($"{Nombre} est estudiando feliz.");
+            }
+
+            public override string ToString()
+            {
+                return $"Estudiante: {Nombre}, Edad: {Edad}";
+            }
+        }
+    }
+}
