5bc2bd1897e66cd4fecf7f60f6ae815180f05e43
Samuel-Fernandez-140923 <samueldukipkoko@gmail.com>
2024-10-17 08:23:43 -0500

    Primera salvada

---
+++ nousar/alumno.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Personas
+{
+    internal class alumno
+    {
+        public void estudia() { Console.WriteLine("Estoy estudiando"); }
+        public void Corre() { Console.WriteLine("Estoy corriendo"); }
+
+
+    }
+}
